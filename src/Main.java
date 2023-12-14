60f0fb24ecaaeb8eb2d47b56ac4029fa9cd1248e
621352 <camarenburt>
2023-12-14 09:33:33 -0500

    reg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
+import java.util.Scanner;
+public class RegExMagic {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your SSN is: " + InputHelper.getRegExString(scan, "Enter your SSN", "\\d{3}-\\d{2}-\\d{4}"));
+        System.out.println("Your Student M Number is: " + InputHelper.getRegExString(scan, "Enter your Student M Number", "(M|m)\\d{5}"));
+        System.out.println("You choose: " + InputHelper.getRegExString(scan, "Menu:  O - Open  S - Save  V - View  Q - Quit", "[OoSsVvQq]"));
     }
 }
